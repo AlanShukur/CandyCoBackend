eae972d80dbd6517a77ca7f13199bcf355447fc2
Top-shagger <alan_shukur1@hotmail.com>
2024-11-28 04:51:53 +0100

    add customer model

--- src/main/java/candyStore/BackendExam/model/Customer.java
+++ src/main/java/candyStore/BackendExam/model/Customer.java
@@ -1,4 +1,24 @@
 package candyStore.BackendExam.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "customer_gen")
+    @SequenceGenerator(name = "customer_gen", sequenceName = "customer_seq", allocationSize = 1)
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+
 }
