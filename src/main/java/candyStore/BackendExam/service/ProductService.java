cad30c238a250a44b8c73c177663018b12ba8568
Top-shagger <alan_shukur1@hotmail.com>
2024-11-28 04:53:09 +0100

    add product service logic

--- src/main/java/candyStore/BackendExam/service/ProductService.java
+++ src/main/java/candyStore/BackendExam/service/ProductService.java
@@ -1,4 +1,24 @@
 package candyStore.BackendExam.service;
 
+import candyStore.BackendExam.model.Product;
+import candyStore.BackendExam.repository.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+    private final ProductRepo productRepo;
+
+    @Autowired
+    public ProductService(ProductRepo productRepo) {
+        this.productRepo = productRepo;
+    }
+    public Product saveProduct(Product product){
+        return productRepo.save(product);
+    }
+    public List<Product> getProducts() {
+        return productRepo.findAll();
+    }
 }
