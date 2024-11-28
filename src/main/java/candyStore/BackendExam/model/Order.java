790424a4bbd6a6c57fb246025e46f79ece941185
Top-shagger <alan_shukur1@hotmail.com>
2024-11-28 04:52:10 +0100

    add order model

--- src/main/java/candyStore/BackendExam/model/Order.java
+++ src/main/java/candyStore/BackendExam/model/Order.java
@@ -1,4 +1,23 @@
 package candyStore.BackendExam.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Map;
+
+@Getter @Setter
+
+@NoArgsConstructor
+@AllArgsConstructor
 public class Order {
+    private long id;
+    private Map<Product, Integer> products;
+    private int shippingCharge;
+    private int totalPrice;
+    private String shipped;
+    private String shippingAddress;
+
 }
