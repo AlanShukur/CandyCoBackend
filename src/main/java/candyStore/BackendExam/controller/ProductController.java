e12c5a1b6a676d5f639bdced54fd6be289458254
Top-shagger <alan_shukur1@hotmail.com>
2024-11-28 04:52:37 +0100

    add product controller

--- src/main/java/candyStore/BackendExam/controller/ProductController.java
+++ src/main/java/candyStore/BackendExam/controller/ProductController.java
@@ -1,4 +1,30 @@
 package candyStore.BackendExam.controller;
 
+import candyStore.BackendExam.model.Product;
+import candyStore.BackendExam.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
 public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getProducts() {
+        return productService.getProducts();
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> saveProduct(@RequestBody Product product) {
+        Product savedProduct = productService.saveProduct(product);
+        return ResponseEntity.ok(savedProduct);
+    }
 }
