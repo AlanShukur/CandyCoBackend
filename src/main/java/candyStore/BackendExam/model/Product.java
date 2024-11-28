3b5000c71e1b092067e1bef1ffc922dfee12113b
Top-shagger <alan_shukur1@hotmail.com>
2024-11-28 04:52:28 +0100

    add product model

--- src/main/java/candyStore/BackendExam/model/Product.java
+++ src/main/java/candyStore/BackendExam/model/Product.java
@@ -1,4 +1,25 @@
 package candyStore.BackendExam.model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String description;
+    private int price;
+    private String status;
+    private int quantity;
+
 }
