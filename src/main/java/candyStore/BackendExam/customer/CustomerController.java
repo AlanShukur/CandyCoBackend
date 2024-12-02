86b9d4d103f6f9c38279e4155e7eab1377837df9
Zarrar <zaah004@student.kristiania.no>
2024-12-02 20:53:02 +0100

    very tiny fix in unused imports

--- src/main/java/candyStore/BackendExam/customer/CustomerController.java
+++ src/main/java/candyStore/BackendExam/customer/CustomerController.java
@@ -1,7 +1,5 @@
 package candyStore.BackendExam.customer;
 
-
-import candyStore.BackendExam.product.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
