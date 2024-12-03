45c159c7d241605247314c52eabf1aa82b978567
Top-shagger <alan_shukur1@hotmail.com>
2024-12-03 23:58:51 +0100

    Added controller logic to orders

--- src/main/java/candyStore/BackendExam/order/OrderController.java
+++ src/main/java/candyStore/BackendExam/order/OrderController.java
@@ -1,4 +1,30 @@
 package candyStore.BackendExam.order;
 
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/order")
 public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping
+    public List<Order> getOrders() {
+        return orderService.getOrders();
+    }
+
+    @PostMapping(consumes = "application/json")
+    public ResponseEntity<Order> saveOrder(@RequestBody Order order) {
+        Order savedOrder = orderService.saveOrder(order);
+        return ResponseEntity.ok(savedOrder);
+    }
 }
