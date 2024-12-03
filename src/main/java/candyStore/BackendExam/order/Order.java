d00f533db78aae803605d265267d5b1a444436da
Top-shagger <alan_shukur1@hotmail.com>
2024-12-03 23:58:18 +0100

    Created database logic for orders

--- src/main/java/candyStore/BackendExam/order/Order.java
+++ src/main/java/candyStore/BackendExam/order/Order.java
@@ -1,23 +1,42 @@
 package candyStore.BackendExam.order;
 
-import candyStore.BackendExam.product.Product;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.Map;
+import java.util.*;
 
 @Getter @Setter
-
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "orders")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "order_gen")
+    @SequenceGenerator(name = "order_gen", sequenceName = "order_seq", allocationSize = 1)
     private long id;
-    private Map<Product, Integer> products;
+
+    @ElementCollection
+    @CollectionTable(name = "order_products", joinColumns = @JoinColumn(name = "order_id"))
+    private List<ProductQuantity> products = new ArrayList<>();
+
     private int shippingCharge;
     private int totalPrice;
     private String shipped;
     private String shippingAddress;
+
+    @Embeddable
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class ProductQuantity {
+        private Long productId;
+        private Integer quantity;
+
+    }
 
 }
